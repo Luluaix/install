6f39111deb207ca66e881866a9aba27d30d134cf
Philippe DAMOUNE <pdamoune@e3r1p4.42.fr>
2017-02-21 21:28:39 +0100

    update

---
+++ install.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/Luluaix/42.git ~/Documents/42
+git clone https://github.com/Luluaix/dotfiles.git ~/Documents/42/config
+export ZSH="~/Documents/42/config/.dotfiles/.oh-my-zsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
