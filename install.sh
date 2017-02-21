59ad130e2572a0d476c3e7a85332e1f7bc1ca76a
Philippe DAMOUNE <pdamoune@e3r1p4.42.fr>
2017-02-22 00:20:53 +0100

    update PATH HOME

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 # sh -c "$(curl -fsSL https://raw.githubusercontent.com/Luluaix/install/master/install.sh)"
-git clone https://github.com/Luluaix/42.git ~/Documents/42
-git clone https://github.com/Luluaix/dotfiles.git ~/Documents/42/config
+git clone https://github.com/Luluaix/42.git $HOME/Documents/42
+git clone https://github.com/Luluaix/dotfiles.git $HOME/Documents/42/config
+
 export ZSH="$HOME/Documents/42/config/.oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 rm -Rf ~/.brew
@@ -11,3 +12,4 @@ ln -sF ~/Documents/42/config/.brew ~/.brew
 ln -sF ~/Documents/42/config/.atom ~/.atom
 ln -s ~/Documents/42/config/.zshrc ~/.zshrc
 ln -s ~/Documents/42/config/.vimrc ~/.vimrc
+
