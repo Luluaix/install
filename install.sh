d1224ddf3e899d120968a5a44f2e640f3e66aae2
Philippe DAMOUNE <pdamoune@e3r1p4.42.fr>
2017-02-21 21:53:54 +0100

    update

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/Luluaix/install/master/install.sh)"
 git clone https://github.com/Luluaix/42.git ~/Documents/42
 git clone https://github.com/Luluaix/dotfiles.git ~/Documents/42/config
-export ZSH="$HOME/Documents/42/config/.dotfiles/.oh-my-zsh"
+export ZSH="$HOME/Documents/42/config/.oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
