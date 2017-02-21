37b3b0e735503a4065897c52b4f4d727ba3761c2
Luluaix <pdamoune@student.42.fr>
2017-02-21 23:56:04 +0100

    update lien symboliques

--- install.sh
+++ install.sh
@@ -3,4 +3,11 @@ git clone https://github.com/Luluaix/42.git ~/Documents/42
 git clone https://github.com/Luluaix/dotfiles.git ~/Documents/42/config
 export ZSH="$HOME/Documents/42/config/.oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
+# rm -Rf ~/.brew
+rm -Rf ~/.atom
+rm ~/.zshrc
+rm ~/.vimrc
+#ln -sF ~/Documents/42/config/.brew ~/.brew
+ln -sF ~/Documents/42/config/.atom ~/.atom
+ln -s ~/Documents/42/config/.zshrc ~/.zshrc
+ln -s ~/Documents/42/config/.vimrc ~/.vimrc
