dddc972a5f3bce6ea98061e37b5f9d5514e93269
Philippe DAMOUNE <pdamoune@e3r1p4.42.fr>
2017-02-22 01:00:05 +0100

    maj clone

--- install.sh
+++ install.sh
@@ -1,15 +1,22 @@
 # sh -c "$(curl -fsSL https://raw.githubusercontent.com/Luluaix/install/master/install.sh)"
-git clone https://github.com/Luluaix/42.git $HOME/Documents/42
-git clone https://github.com/Luluaix/dotfiles.git $HOME/Documents/42/config
-
-export ZSH="$HOME/Documents/42/config/.oh-my-zsh"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-rm -Rf ~/.brew
-rm -Rf ~/.atom
-rm ~/.zshrc
-rm ~/.vimrc
-ln -sF ~/Documents/42/config/.brew ~/.brew
-ln -sF ~/Documents/42/config/.atom ~/.atom
-ln -s ~/Documents/42/config/.zshrc ~/.zshrc
-ln -s ~/Documents/42/config/.vimrc ~/.vimrc
-
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+export PROG="$HOME/Documents/42"
+export ZSH="$PROG/config/.oh-my-zsh"
+git clone https://github.com/Luluaix/42.git 				$PROG
+git clone https://github.com/Luluaix/dotfiles.git 			$PROG/config
+git clone https://github.com/Luluaix/documents.git 			$PROG/documents
+git clone https://github.com/Luluaix/test_functions.git 	$PROG/projets_42/test_functions
+git clone https://github.com/Luluaix/library.git 			$PROG/projets_42/library
+git clone https://github.com/Luluaix/ft_printf.git			$PROG/projets_42/algo/ft_printf
+git clone https://github.com/Luluaix/get_next_line.git 		$PROG/projets_42/preambule/get_next_line
+git clone https://github.com/Luluaix/fillit.git 			$PROG/projets_42/preambule/fillit
+git clone https://github.com/Luluaix/libft.git				$PROG/projets_42/preambule/libft
+git clone https://github.com/Luluaix/piscine_reloaded.git	$PROG/projets_42/preambule/piscine_reloaded
+rm -Rf $HOME/.brew
+rm -Rf $HOME/.atom
+rm $HOME/.zshrc
+rm $HOME/.vimrc
+ln -sF $PROG/config/.brew $HOME/.brew
+ln -sF $PROG/config/.atom $HOME/.atom
+ln -s $PROG/config/.zshrc $HOME/.zshrc
+ln -s $PROG/config/.vimrc $HOME/.vimrc
